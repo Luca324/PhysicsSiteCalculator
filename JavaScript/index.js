e544d7d001be04f932f3d92fc9a0569da04927c2
Luca324 <tanya.mosol@yandex.ru>
2024-06-02 16:57:29 +0300

    2.0
    animations (burger, hover)
    footer

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     if (counter%2 == 1) { $('#menu').hide() }
+    $('footer').html('<p><span class="top">© Мосолова Т.А., Андреева Е.А., 2024. All rights reserved.        <a href="https://github.com/Luca324/PhysicsSiteCalculator">            <img src="img/github-mark-white.svg"><span>        github</span>        </a>    </span>    </p><p> Этот сайт создан в учебных целях и предназначен для свободного некоммерческого использования. Вы можете        использовать код, дизайн и контент сайта при условии сохранения авторства. Коммерческое использование сайта без        разрешения владельца запрещено.</p>')
+
+    
     
     $('#menubutton').click(function() {
-        console.log('you pressed the button')
 
         if (counter%2 == 0) {
             $('#menu').hide()
@@ -10,19 +12,19 @@ $(document).ready(function() {
             $('header').show()
 
             $('body').css({'background': '#FAF6EB'})
-            $('#svgbutton').css({'background': '#FAF6EB', 'fill': '#1F3439'})
-            
-            console.log('work, header - show; menu - hide')
         } else {
             $('#menu').show()
             $("#work").hide()
             $('header').hide()
 
             $('body').css({'background': '#1F3439'})
-            $('#svgbutton').css({'background': '#1F3439', 'fill': '#FAF6EB'})
-
-            console.log('work, header - hide; menu - show')
         }
+
+        if (counter != 1) {
+            $('#inner').toggleClass('disactive')
+        }
+        $('#inner').toggleClass('active');
+
         counter += 1
     })
 })
